34b649eb81d4bed1fc95dc57344199ae88bf3ae0
Mahmoud <mahmoudabdelrahem74@gmail.com>
2024-02-17 17:19:45 +0200

    Committing local changes to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Return_;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 
 
