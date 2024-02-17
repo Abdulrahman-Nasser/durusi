9bfc900cecc4ff0b64abfd58f080256597fd3d74
Mahmoud <mahmoudabdelrahem74@gmail.com>
2024-02-17 20:56:38 +0200

    Committing local changes to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthApiController\authController;
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use PhpParser\Node\Stmt\Return_;
@@ -10,6 +11,8 @@ use PhpParser\Node\Stmt\Return_;
 
 Route::post("/register", [authController::class, "register"]);
 Route::post('login' , [authController::class , 'login']);
+Route::get('auth/google',[SocialiteController::class , 'redirectToGoogle'])->middleware('web');
+Route::get('auth/google/callback',[SocialiteController::class , 'handelGoogleCallBack'])->middleware('web');
 Route::middleware("auth:sanctum")->group(function () {
     Route::get('/logout', [authController::class, 'logout']);
     Route::get("/home", function () {
